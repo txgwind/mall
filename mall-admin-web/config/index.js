09d1971e5d1909f25930ab1505058b056cae2fd4
txgwind <157508792@qq.com>
2019-10-02 16:18:42 +0800

    afe

--- mall-admin-web/config/index.js
+++ mall-admin-web/config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/java/mall-admin/',
+    assetsPublicPath: '/1111',
 
     /**
      * Source Maps
